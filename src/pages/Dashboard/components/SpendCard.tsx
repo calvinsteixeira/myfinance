6bf35f0adb34cb0f983a150492e313cde13af426
Calvin <calvin@MacBook-Air-de-Calvin.local>
2025-01-30 21:02:18 -0300

    Realizando melhoria de desempenho

--- src/pages/Dashboard/components/SpendCard.tsx
+++ src/pages/Dashboard/components/SpendCard.tsx
@@ -1,62 +1,57 @@
 import React from "react";
 import { useDashboard } from "../context";
 
-type SpendStatus = {
-  message: string;
-  goalStatus: "positive" | "negative";
-};
-
 export default function SpendCard() {
   const { spendCard } = useDashboard();
-  const [spendStatus, setSpendStatus] = React.useState<SpendStatus>({} as SpendStatus);
 
   const calculateGoalStatusMessage = (
     totalSpend: number,
     goal: number
   ): {
     message: string;
+    messageColor: string;
     goalStatus: "positive" | "negative";
   } => {
     switch (true) {
       case totalSpend > goal:
         return {
           message: `R$ ${totalSpend - goal} fora da meta`,
+          messageColor: "text-destructive",
           goalStatus: "negative",
         };
         break;
       default:
         return {
           message: "",
+          messageColor: "text-primary",
           goalStatus: "positive",
         };
         break;
     }
   };
 
-  React.useEffect(() => {
-    const value = calculateGoalStatusMessage(spendCard.total, spendCard.goal);
-
-    setSpendStatus({
-      message: value.message,
-      goalStatus: value.goalStatus,
-    });
-  }, [spendCard]);
-
-  const statusMessageColor =
-    spendStatus.goalStatus == "positive" ? "text-primary" : "text-destructive";
+  const memoizedGoalStatusMessage = React.useMemo(() => {
+    return calculateGoalStatusMessage(spendCard.total, spendCard.goal);
+  }, [spendCard.goal, spendCard.total]);
 
   return (
     <div aria-labelledby={"spend-card-title"} className={"bg-zinc-900 px-4 py-6 w-full rounded-md"}>
       <strong id={"spend-card-title"}>GASTOS DO MÊS</strong>
       <div className={"mt-2"}>
-        <p aria-label={"total-spend"} className={`text-3xl font-bold ${statusMessageColor}`}>
+        <p
+          aria-label={"total-spend"}
+          className={`text-3xl font-bold ${memoizedGoalStatusMessage.messageColor}`}
+        >
           R$ {spendCard.total}
         </p>
         <p aria-label={"month-goal"} className={"text-sm font-medium"}>
           META: R$ {spendCard.goal}
         </p>
-        <p aria-label={"month-goal-status"} className={`text-sm ${statusMessageColor}`}>
-          {spendStatus.message}
+        <p
+          aria-label={"month-goal-status"}
+          className={`text-sm ${memoizedGoalStatusMessage.messageColor}`}
+        >
+          {memoizedGoalStatusMessage.message}
         </p>
       </div>
     </div>
